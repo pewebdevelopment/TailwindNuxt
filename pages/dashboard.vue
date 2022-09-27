598cbd64de609e9e59c6565dbc86733fb0c60744
peiacademics <acad.paathshala@gmail.com>
2022-09-27 16:38:06 +0530

    barebones dashboard, working with nuxtlayout

---
+++ pages/dashboard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed, ref, onMounted } from "vue";
+import { useMainStore } from "@/stores/main.js";
+import {
+  mdiAccountMultiple,
+  mdiCartOutline,
+  mdiChartTimelineVariant,
+  mdiMonitorCellphone,
+  mdiReload,
+  mdiGithub,
+  mdiChartPie,
+} from "@mdi/js";
+import * as chartConfig from "@/components/Charts/chart.config.js";
+
+const chartData = ref(null);
+const fillChartData = () => {
+  chartData.value = chartConfig.sampleChartData();
+};
+onMounted(() => {
+  fillChartData();
+});
+const mainStore = useMainStore();
+const clientBarItems = computed(() => mainStore.clients.slice(0, 4));
+const transactionBarItems = computed(() => mainStore.history);
+</script>
+
+<template>
+  <div>
+    <NuxtLayout name="Authenticated.vue"> This is the dashboard </NuxtLayout>
+  </div>
+</template>
