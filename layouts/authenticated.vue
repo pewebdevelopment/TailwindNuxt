a2682d2427643957e7a717a8ca12956183c9f31f
peiacademics <acad.paathshala@gmail.com>
2022-09-27 16:55:25 +0530

    barebones dashboard working

---
+++ layouts/authenticated.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { mdiForwardburger, mdiBackburger, mdiMenu } from "@mdi/js";
+import { ref } from "vue";
+//import { useRouter } from "vue-router";
+import menuAside from "@/menuAside.js";
+import menuNavBar from "@/menuNavBar.js";
+import { useMainStore } from "@/stores/main.js";
+import { useStyleStore } from "@/stores/style.js";
+import BaseIcon from "@/components/BaseIcon.vue";
+import FormControl from "@/components/FormControl.vue";
+
+import NavBarItemPlain from "@/components/NavBarItemPlain.vue";
+import AsideMenu from "@/components/AsideMenu.vue";
+import FooterBar from "@/components/FooterBar.vue";
+useMainStore().setUser({
+  name: "John Doe",
+  email: "john@example.com",
+  avatar:
+    "https://avatars.dicebear.com/api/avataaars/example.svg?options[top][]=shortHair&options[accessoriesChance]=93",
+});
+const layoutAsidePadding = "xl:pl-60";
+const styleStore = useStyleStore();
+//const router = useRouter();
+const isAsideMobileExpanded = ref(false);
+const isAsideLgActive = ref(false);
+router.beforeEach(() => {
+  isAsideMobileExpanded.value = false;
+  isAsideLgActive.value = false;
+});
+const menuClick = (event, item) => {
+  if (item.isToggleLightDark) {
+    styleStore.setDarkMode();
+  }
+  if (item.isLogout) {
+    //
+  }
+};
+</script>
+
+<template></template>
