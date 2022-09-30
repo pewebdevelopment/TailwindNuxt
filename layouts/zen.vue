f1f3911563fb242914cddf0d1129079aa08f511c
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-09-30 11:05:05 +0530

    completed the code in mainStore

--- layouts/zen.vue
+++ layouts/zen.vue
@@ -14,12 +14,12 @@ import NavBarItemPlain from "@/components/NavBarItemPlain.vue";
 import AsideMenu from "@/components/AsideMenu.vue";
 import FooterBar from "@/components/FooterBar.vue";
 
-// useMainStore().setUser({
-//   name: "John Doe",
-//   email: "john@example.com",
-//   avatar:
-//     "https://avatars.dicebear.com/api/avataaars/example.svg?options[top][]=shortHair&options[accessoriesChance]=93",
-// });
+useMainStore().setUser({
+  name: "John Doe",
+  email: "john@example.com",
+  avatar:
+    "https://avatars.dicebear.com/api/avataaars/example.svg?options[top][]=shortHair&options[accessoriesChance]=93",
+});
 const layoutAsidePadding = "xl:pl-60";
 const styleStore = useStyleStore();
 const router = useRouter();
