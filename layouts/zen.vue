88cad481790ed4a479bf8942d9711b6817363385
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-09-30 11:10:02 +0530

    NavBar working

--- layouts/zen.vue
+++ layouts/zen.vue
@@ -26,29 +26,29 @@ const router = useRouter();
 const isAsideMobileExpanded = ref(false);
 const isAsideLgActive = ref(false);
 router.beforeEach(() => {
-    isAsideMobileExpanded.value = false;
-    isAsideLgActive.value = false;
+  isAsideMobileExpanded.value = false;
+  isAsideLgActive.value = false;
 });
 const menuClick = (event, item) => {
-    if (item.isToggleLightDark) {
-        styleStore.setDarkMode();
-    }
-    if (item.isLogout) {
-        //
-    }
+  if (item.isToggleLightDark) {
+    styleStore.setDarkMode();
+  }
+  if (item.isLogout) {
+    //
+  }
 };
 
 </script>
 
 <template>
-    <div>
-        <div :class="{
-          dark: styleStore.darkMode,
-          'overflow-hidden lg:overflow-visible': isAsideMobileExpanded,
-        }">
-        
+  <div>
+    <div :class="{
+      dark: styleStore.darkMode,
+      'overflow-hidden lg:overflow-visible': isAsideMobileExpanded,
+    }">
 
-            <div :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': isAsideMobileExpanded }]" class="
+
+      <div :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': isAsideMobileExpanded }]" class="
         pt-14
         min-h-screen
         w-screen
@@ -58,54 +58,35 @@ const menuClick = (event, item) => {
         dark:bg-slate-800 dark:text-slate-100
       ">
 
-      <NavBarItemPlain
-          display="flex lg:hidden"
-          @click.prevent="isAsideMobileExpanded = !isAsideMobileExpanded"
-        >
-          <BaseIcon
-            :path="isAsideMobileExpanded ? mdiBackburger : mdiForwardburger"
-            size="24"
-          />
-        </NavBarItemPlain>
-        <NavBarItemPlain
-          display="hidden lg:flex xl:hidden"
-          @click.prevent="isAsideLgActive = true"
-        >
-          <BaseIcon :path="mdiMenu" size="24" />
-        </NavBarItemPlain>
-        <NavBarItemPlain use-margin>
-          <FormControl
-            placeholder="Search (ctrl+k)"
-            ctrl-k-focus
-            transparent
-            borderless
-          />
-        </NavBarItemPlain>
+        <NavBar :menu="menuNavBar" :class="[
+          layoutAsidePadding,
+          { 'ml-60 lg:ml-0': isAsideMobileExpanded },
+        ]" @menu-click="menuClick">
 
-        <AsideMenu
-        :is-aside-mobile-expanded="isAsideMobileExpanded"
-        :is-aside-lg-active="isAsideLgActive"
-        :menu="menuAside"
-        @menu-click="menuClick"
-        @aside-lg-close-click="isAsideLgActive = false"
-      />
+          <NavBarItemPlain display="flex lg:hidden" @click.prevent="isAsideMobileExpanded = !isAsideMobileExpanded">
+            <BaseIcon :path="isAsideMobileExpanded ? mdiBackburger : mdiForwardburger" size="24" />
+          </NavBarItemPlain>
+          <NavBarItemPlain display="hidden lg:flex xl:hidden" @click.prevent="isAsideLgActive = true">
+            <BaseIcon :path="mdiMenu" size="24" />
+          </NavBarItemPlain>
+          <NavBarItemPlain use-margin>
+            <FormControl placeholder="Search (ctrl+k)" ctrl-k-focus transparent borderless />
+          </NavBarItemPlain>
+        </NavBar>>
+        <AsideMenu :is-aside-mobile-expanded="isAsideMobileExpanded" :is-aside-lg-active="isAsideLgActive"
+          :menu="menuAside" @menu-click="menuClick" @aside-lg-close-click="isAsideLgActive = false" />
 
-            </div>
+      </div>
 
 
-            <slot />
+      <slot />
 
-            <FooterBar>
+      <FooterBar>
         Get more with
-        <a
-          href="#"
-          target="_blank"
-          class="text-blue-600"
-          >Photon Ecademy</a
-        >
+        <a href="#" target="_blank" class="text-blue-600">Photon Ecademy</a>
       </FooterBar>
-        </div>
-
-
     </div>
+
+
+  </div>
 </template>
