c63b401a499ea1442a1350aac790ef83377675df
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-10-01 13:57:53 +0530

    Trying with styleview to apply darkmode

--- stores/style.js
+++ stores/style.js
@@ -3,7 +3,8 @@ import * as styles from "@/configs/styles";
 import { darkModeKey, styleKey } from "@/configs/config";
 
 
-  const state = () => ({
+export const useStyleStore = defineStore("style", {
+  state: () => ({
     /* Styles */
     asideStyle: "",
     asideScrollbarsStyle: "",
@@ -18,11 +19,8 @@ import { darkModeKey, styleKey } from "@/configs/config";
 
     /* Dark mode */
     darkMode: false,
-  });
-
-
-
-  const actions = {
+  }),
+  actions: {
     setStyle(payload) {
       if (!styles[payload]) {
         return;
@@ -56,11 +54,98 @@ import { darkModeKey, styleKey } from "@/configs/config";
         );
       }
     },
-  };
-
-
-
-export const useStyleStore = defineStore('style', {
-  state,
-  actions
+  },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const state = () => ({
+  //   /* Styles */
+  //   asideStyle: "",
+  //   asideScrollbarsStyle: "",
+  //   asideBrandStyle: "",
+  //   asideMenuItemStyle: "",
+  //   asideMenuItemActiveStyle: "",
+  //   asideMenuDropdownStyle: "",
+  //   navBarItemLabelStyle: "",
+  //   navBarItemLabelHoverStyle: "",
+  //   navBarItemLabelActiveColorStyle: "",
+  //   overlayStyle: "",
+
+  //   /* Dark mode */
+  //   darkMode: false,
+  // });
+
+
+
+
+
+
+
+
+//   const actions = {
+//     setStyle(payload) {
+//       if (!styles[payload]) {
+//         return;
+//       }
+
+//       if (typeof localStorage !== "undefined") {
+//         localStorage.setItem(styleKey, payload);
+//       }
+
+//       const style = styles[payload];
+
+//       for (const key in style) {
+//         this[`${key}Style`] = style[key];
+//       }
+//     },
+
+//     setDarkMode(payload = null) {
+//       this.darkMode = payload !== null ? payload : !this.darkMode;
+
+//       if (typeof localStorage !== "undefined") {
+//         localStorage.setItem(darkModeKey, this.darkMode ? "1" : "0");
+//       }
+
+//       if (typeof document !== "undefined") {
+//         document.body.classList[this.darkMode ? "add" : "remove"](
+//           "dark-scrollbars"
+//         );
+
+//         document.documentElement.classList[this.darkMode ? "add" : "remove"](
+//           "dark-scrollbars-compat"
+//         );
+//       }
+//     },
+//   };
+
+
+
+// export const useStyleStore = defineStore('style', {
+//   state,
+//   actions
+// });
