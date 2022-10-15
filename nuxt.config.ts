f8604bec8c9b602c447e78965f2e48b5dc30660d
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-10-15 17:24:46 +0530

    Took the latest working Nuxt Repo

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+
+
+
+export default defineNuxtConfig({
+  srcDir: "./",
+  css: [
+    '@/assets/css/main.css',
+  ],
+
+  buildModules: [
+    '@pinia/nuxt',
+    
+  ],
+
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
+
+// // https://v3.nuxtjs.org/api/configuration/nuxt.config
+// export default defineNuxtConfig({
+// modules: [
+//     '@nuxtjs/tailwindcss'
+//   ]
+// })
