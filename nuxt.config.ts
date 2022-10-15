aa0c82d8a1c1a98b49b8d299004c1460996a9af2
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-10-15 17:26:09 +0530

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,10 +23,3 @@ export default defineNuxtConfig({
     },
   },
 });
-
-// // https://v3.nuxtjs.org/api/configuration/nuxt.config
-// export default defineNuxtConfig({
-// modules: [
-//     '@nuxtjs/tailwindcss'
-//   ]
-// })
